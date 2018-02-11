9d07036ef26b72bbd9ef488d6fa33900959eac00
Kevin <kevindejesus2012@hotmail.com>
2018-02-11 16:31:17 -0600

    clase1

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 
-/**
- * Created by PhpStorm.
- * User: Lenovo i7
- * Date: 11/02/2018
- * Time: 03:32 PM
- */
+
+
+  <header style="background-color: blanchedalmond; width: 100%; height: 200px;">
+
+      <h3 style='text-align:center;'"color:Tomato;">Hello World</h3>
+      <section style="background-color: black; width: 100%; height: 150px;">
+      </section>
+
+      <nav></nav>
+
+  </header>
+</body>
+</html>
